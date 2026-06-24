111d56fcef7e53794fdf43ffb78f7bc9f9cdaa61
pedroabreuu <99131505+pedroabreuu@users.noreply.github.com>
2026-06-24 07:16:20 -0300

    fix: adicionando type hints

--- template.py
+++ template.py
@@ -1,31 +1,31 @@
 from abc import ABC, abstractmethod
-
+from typing import Any
 
 class OrdenadaPalavras(ABC):
 
-    def executar(self, palavras):
+    def executar(self, palavras: list[str]) -> None:
 
         self.vazio(palavras)
         palavras.sort(key=self.regra_ordenacao)
 
-    def vazio(self, palavras):
+    def vazio(self, palavras: list[str]) -> None:
         if not palavras:
             raise ValueError("Lista de palavras vazia.")
 
     @abstractmethod
-    def regra_ordenacao(self, palavra):
+    def regra_ordenacao(self, palavra: str) -> Any:
         pass
 
 
 class OrdenadaUltimaLetra(OrdenadaPalavras):
 
-    def regra_ordenacao(self, palavra):
+    def regra_ordenacao(self, palavra:str) -> tuple[str, str]:
         #Ordena pela última letra e o critério de desempate é o restante da palavra 
         return (palavra[-1].lower(), palavra.lower())
 
 class OrdenadaTamanho(OrdenadaPalavras):
     
-    def regra_ordenacao(self, palavra):
+    def regra_ordenacao(self, palavra: str) -> tuple[int, str]:
         return (len(palavra), palavra.lower())
 
 
