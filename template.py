d3eb0821a661caceec4d12dd1dd16cb680de901a
anabiagarcia <anabia.garcia29@gmail.com>
2026-06-24 21:20:09 -0300

    add: Ordem alfabética

--- template.py
+++ template.py
@@ -1,31 +1,42 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
+class OrdenaPalavras(ABC):
 
-class OrdenadaPalavras(ABC):
-
-    def executar(self, palavras):
+    def executar(self, palavras: list[str]) -> None:
 
         self.vazio(palavras)
         palavras.sort(key=self.regra_ordenacao)
 
-    def vazio(self, palavras):
+    def vazio(self, palavras: list[str]) -> None:
         if not palavras:
             raise ValueError("Lista de palavras vazia.")
 
     @abstractmethod
-    def regra_ordenacao(self, palavra):
+    def regra_ordenacao(self, palavra: str) -> Any:
         pass
 
+class OrdenaAlfabetica(OrdenaPalavras):
 
-class OrdenadaUltimaLetra(OrdenadaPalavras):
+    def regra_ordenacao(self, palavra: str) -> tuple[str, str]:
+        # Ordena em ordem alfabética, em caso de empate, usa a palavra inteira.
+        return (palavra[0].lower(), palavra.lower())
 
-    def regra_ordenacao(self, palavra):
-        #Ordena pela última letra e o critério de desempate é o restante da palavra 
+class OrdenaUltimaLetra(OrdenaPalavras):
+
+    def regra_ordenacao(self, palavra: str) -> tuple[str, str]:
+        # Ordena pela última letra, em caso de empate, usa a palavra inteira.
         return (palavra[-1].lower(), palavra.lower())
 
+class OrdenaTamanho(OrdenaPalavras):
 
-# Teste da última letra 
-palavras = [
+    def regra_ordenacao(self, palavra: str) -> tuple[int, str]:
+        # Ordena pelo tamanho da palavra, em caso de empate, usa a palavra inteira.
+        return (len(palavra), palavra.lower())
+
+
+# Exemplos de uso
+palavras_1 = [
     "ana",
     "beatriz",
     "mario",
@@ -34,6 +45,21 @@ palavras = [
     "camile",
     "adilson"
 ]
-ordenador = OrdenadaUltimaLetra()
-ordenador.executar(palavras)
-print(palavras)
+
+palavras_2 = palavras_1.copy()
+palavras_3 = palavras_1.copy()
+
+print("Ordenada pelo critério: Ordem alfabética")
+ordenador = OrdenaAlfabetica()
+ordenador.executar(palavras_1)
+print(palavras_1)
+
+print("Ordenada pelo critério: Ordem alfabética da última letra")
+ordenador = OrdenaUltimaLetra()
+ordenador.executar(palavras_2)
+print(palavras_2)
+
+print("Ordenada pelo critério: Tamanho do texto")
+ordenador = OrdenaTamanho()
+ordenador.executar(palavras_3)
+print(palavras_3)
